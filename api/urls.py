cc01fdbaeac3eef8bebedf3d2b3ec291009f6c4c
Isaí Ayala César <isaiacwebdev@gmail.com>
2023-10-19 15:38:22 -0600

    feat: add urls

--- api/urls.py
+++ api/urls.py
@@ -14,9 +14,42 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+from django.views.static import serve
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+from .views import api, jwt, user
+
+
+router = DefaultRouter()
+
+router.register(r'users', user.UserViewSet)
+
+urlpatterns = (
+    [
+        path('admin/', admin.site.urls),
+        re_path('^ping/', api.ping),
+        re_path(
+            '^auth/login/', jwt.JWTObtainPairView.as_view(), name="token_obtain_pair"
+        ),
+        re_path(
+            '^auth/token/refresh/',
+            jwt.JWTRefreshView.as_view(),
+            name="token_refresh",
+        ),
+        re_path('^', include(router.urls)),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
+
+
+if settings.APP_ENV == 'local' and not settings.DEBUG:
+    urlpatterns.append(
+        re_path(
+            r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}
+        )
+    )
